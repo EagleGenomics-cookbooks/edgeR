82419291070453b751274cec0bf3e44f696c4a46
craig mcanulla <craig.mcanulla@eaglegenomics.com>
2020-04-24 14:57:35 +0100

    Initial commit edgeR nstalls and tests pass

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,4 +6,12 @@
 
 include_recipe 'R'
 
+execute 'Rscript -e \'BiocManager::install(version = "3.10", ask=FALSE)\''
 execute 'Rscript -e \'BiocManager::install("edgeR")\''
+
+# Bioconductor version 3.10 is linked to edgeR version 3.28.1
+# N.B. We cannot control the Bioconductor package versions - they are linked to the Bioconductor version
+magic_shell_environment 'EDGER_VERSION' do
+  value '3.28.1'
+end
+
